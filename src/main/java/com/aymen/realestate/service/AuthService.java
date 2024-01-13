83622e24425ad14a989e095ad48424575356d30e
Aymen Nacer <aymen@nacer.com>
2024-01-13 15:59:50 +0100

    Add Auth Service

--- src/main/java/com/aymen/realestate/service/AuthService.java
+++ src/main/java/com/aymen/realestate/service/AuthService.java
@@ -1,2 +1,19 @@
-package com.aymen.realestate.service;public class AuthService {
+package com.aymen.realestate.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthService {
+
+    public void signUp(String username, String email, String password) {
+    }
+
+    public void signIn(String email, String password) {
+    }
+
+    public void googleSignIn(String googleToken) {
+    }
+
+    public void signOut() {
+    }
 }
