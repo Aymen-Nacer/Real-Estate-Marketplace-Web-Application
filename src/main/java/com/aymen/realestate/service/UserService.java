e3adebf68cdaeb82f3dc2def793fd9da35653447
Aymen Nacer <aymen@nacer.com>
2024-01-14 11:13:27 +0100

    Add User service

---
+++ src/main/java/com/aymen/realestate/service/UserService.java
@@ -0,0 +1,49 @@
+package com.aymen.realestate.service;
+
+import com.aymen.realestate.dto.UserUpdateRequest;
+import com.aymen.realestate.model.Listing;
+import com.aymen.realestate.model.User;
+import com.aymen.realestate.repository.ListingRepository;
+import com.aymen.realestate.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private ListingRepository listingRepository;
+
+    public User updateUser(Long id, UserUpdateRequest request) {
+        User user = userRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("User not found"));
+
+        user.setUsername(request.getUsername());
+        user.setEmail(request.getEmail());
+
+        return userRepository.save(user);
+    }
+
+    public void deleteUser(Long id) {
+        userRepository.deleteById(id);
+    }
+
+    public List<Listing> getUserListings(Long id) {
+        User user = userRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("User not found"));
+
+        return listingRepository.findByUser(user);
+    }
+
+    public User getUser(Long id) {
+        return userRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("User not found"));
+    }
+
+
+}
